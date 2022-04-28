b88ce03b28c8b5c27a9537af94b966b9f7ad18d5
Eshat ÇELİK <eshat.celik@procenne.com>
2022-04-28 18:11:30 +0300

    added user worker service

---
+++ src/Projections/BlazorSozluk.Projections.FavoriteService/BlazorSozluk.Projections.UsersService/Program.cs
@@ -0,0 +1,10 @@
+using BlazorSozluk.Projections.UsersService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
